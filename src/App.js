f23cce8ac0b5227a2a3a75078a4e476f27719006
Muhangi2 <eliodamutiba@gmail.com>
2024-03-28 23:32:57 +0300

    eachNFT

--- src/App.js
+++ src/App.js
@@ -14,46 +14,44 @@ import Escrow from "./abis/Escrow.json";
 import config from "./config.json";
 
 function App() {
-  //states
-  const [account, setAccount] = useState(null);
   const [provider, setProvider] = useState(null);
   const [escrow, setEscrow] = useState(null);
-  const [homes, setHomes] = useState([]);
 
-  const loadblockchainData = async () => {
-    //provider
+  const [account, setAccount] = useState(null);
+
+  const [homes, setHomes] = useState([]);
+  const [home, setHome] = useState({});
+  const [toggle, setToggle] = useState(false);
+
+  const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     setProvider(provider);
     const network = await provider.getNetwork();
-    console.log(provider);
-    //realestates
+
     const realEstate = new ethers.Contract(
       config[network.chainId].realEstate.address,
       RealEstate,
       provider
     );
     const totalSupply = await realEstate.totalSupply();
-    console.log(realEstate);
-    console.log(totalSupply);
-
     const homes = [];
-    for (var i = 0; i <= totalSupply; i++) {
-      const tokenUrl = await realEstate.TokenURI(i);
-      const response = await fetch(tokenUrl);
-      const metaData = await response.json();
-      homes.push(metaData);
+
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
     }
+
     setHomes(homes);
-    console.log(homes);
-    //escrowcontract
+
     const escrow = new ethers.Contract(
-      config[network.chainId].realEstate.address,
+      config[network.chainId].escrow.address,
       Escrow,
       provider
     );
     setEscrow(escrow);
 
-    //on account change on the metamask
     window.ethereum.on("accountsChanged", async () => {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
@@ -62,36 +60,55 @@ function App() {
       setAccount(account);
     });
   };
-  //useeffect to keeep rnning the fuction on reload
+
   useEffect(() => {
-    loadblockchainData();
+    loadBlockchainData();
   }, []);
+
+  const togglePop = (home) => {
+    setHome(home);
+    toggle ? setToggle(false) : setToggle(true);
+  };
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
       <Search />
+
       <div className="cards__section">
-        <h3>Welcome to Millow</h3>
-        <h2>Places for you</h2>
+        <h3>Homes For You</h3>
+
         <hr />
+
         <div className="cards">
-          <div className="card">
-            <div className="card__image">
-              <img src="" alt="Home" />
+          {homes.map((home, index) => (
+            <div className="card" key={index} onClick={() => togglePop(home)}>
+              <div className="card__image">
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className="card__info">
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> bds |
+                  <strong>{home.attributes[3].value}</strong> ba |
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
             </div>
-            <div className="card__info">
-              <h2>1 ETH</h2>
-              <p>
-                <strong>1</strong>bds
-                <strong>2</strong>bq
-                <strong>3</strong>br
-              </p>
-              <p>1234 Elm st</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
+
+      {toggle && (
+        <Home
+          home={home}
+          provider={provider}
+          account={account}
+          escrow={escrow}
+          togglePop={togglePop}
+        />
+      )}
     </div>
   );
 }
